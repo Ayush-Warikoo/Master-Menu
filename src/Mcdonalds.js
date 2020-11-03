1d9e61119468b78cea00941083413387ce9c8ef7
Ayush Warikoo <ayush.warikoo77@gmail.com>
2020-11-02 22:25:13 -0500

    Organized MenuFilter into its own component

--- src/Mcdonalds.js
+++ src/Mcdonalds.js
@@ -1,42 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./Mcdonalds.css";
 import Product from "./Product";
 import Background from "./img/mcdonalds-banner.jpg";
-import { useStateValue } from "./StateProvider";
+import MenuFilter from "./MenuFilter";
 
 function Mcdonalds() {
-  const [{}, dispatch] = useStateValue();
-
-  const [allergyString, setAllergyString] = useState('');
-  const [preferenceString, setPreferenceString] = useState('');
-  const [budgetString, setBudgetString] = useState(null);
-  const [ratingString, setRatingString] = useState(null);
-
-  const filter = () => {
-    // dispatch the item into the data layer
-    let allergyArray = allergyString.split(",");
-    let preferenceArray = preferenceString.split(",");
-    let budgetValue = budgetString;
-    let ratingValue = ratingString;
-
-    for(let i = 0; i < allergyArray.length; i++)
-    {
-      allergyArray[i] = allergyArray[i].toLowerCase().trim();
-    }
-    for(let i = 0; i < preferenceArray.length; i++)
-    {
-      preferenceArray[i] = preferenceArray[i].toLowerCase().trim();
-    }
-
-    dispatch({
-      type: "UPDATE_FILTER",
-      allergy: allergyArray,
-      preference: preferenceArray,
-      budget: budgetValue,
-      rating: ratingValue
-    });
-  };
-
+  
   return (
 
     <div className="mcdonalds">
@@ -48,24 +17,7 @@ function Mcdonalds() {
           alt="" 
         />
 
-        {/* Personalization Panel*/}
-        <div className="menu__filter">
-          <h1> Menu Filter </h1>
-
-          <h3>Allergies / Dietary Restrictions: </h3>
-          <input type='text' value={allergyString} onChange={e => setAllergyString(e.target.value)} />
-          <h3>Ingredient Preferences:</h3>
-          <input type='text' value={preferenceString} onChange={e => setPreferenceString(e.target.value)} />
-          <h3>Budget Maximum ($):</h3>
-          <input type='text' value={budgetString} onChange={e => setBudgetString(e.target.value)}/>
-          <h3>Rating Minimum (Stars):</h3>
-          <input type='text' value={ratingString} onChange={e => setRatingString(e.target.value)}/>
-
-          <div className="filter__button">
-            <button type='submit' onClick={filter}> Filter </button>
-          </div> 
-
-        </div>
+        <MenuFilter />
 
         {/* Product rows */}
         <div className="mcdonalds__row">
---
+++ src/MenuFilter.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
+function MenuFilter()
+{
+    const [{}, dispatch] = useStateValue();
+
+  const [allergyString, setAllergyString] = useState('');
+  const [preferenceString, setPreferenceString] = useState('');
+  const [budgetString, setBudgetString] = useState(null);
+  const [ratingString, setRatingString] = useState(null);
+
+  const filter = () => {
+    // dispatch the item into the data layer
+    let allergyArray = allergyString.split(",");
+    let preferenceArray = preferenceString.split(",");
+    let budgetValue = budgetString;
+    let ratingValue = ratingString;
+
+    for(let i = 0; i < allergyArray.length; i++)
+    {
+      allergyArray[i] = allergyArray[i].toLowerCase().trim();
+    }
+    for(let i = 0; i < preferenceArray.length; i++)
+    {
+      preferenceArray[i] = preferenceArray[i].toLowerCase().trim();
+    }
+
+    dispatch({
+      type: "UPDATE_FILTER",
+      allergy: allergyArray,
+      preference: preferenceArray,
+      budget: budgetValue,
+      rating: ratingValue
+    });
+  };
+  return(
+    <div className="menu__filter">
+        <h1> Menu Filter </h1>
+
+        <h3>Allergies / Dietary Restrictions: </h3>
+        <input type='text' value={allergyString} onChange={e => setAllergyString(e.target.value)} />
+        <h3>Ingredient Preferences:</h3>
+        <input type='text' value={preferenceString} onChange={e => setPreferenceString(e.target.value)} />
+        <h3>Budget Maximum ($):</h3>
+        <input type='text' value={budgetString} onChange={e => setBudgetString(e.target.value)}/>
+        <h3>Rating Minimum (Stars):</h3>
+        <input type='text' value={ratingString} onChange={e => setRatingString(e.target.value)}/>
+
+        <div className="filter__button">
+            <button type='submit' onClick={filter}> Filter </button>
+        </div> 
+    </div>
+)}
+
+export default MenuFilter
+
